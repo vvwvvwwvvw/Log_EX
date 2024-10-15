aeec8ae49b7e704ef4f901f3f3bf29156ced89bf
남정욱 <wjddnr3304@gmail.com>
2024-10-15 14:22:31 +0900

    각 로직 설명 추가

--- Log_EX/Program.cs
+++ Log_EX/Program.cs
@@ -23,13 +23,13 @@ namespace Log_EX
 
         public void CreateLogFile()
         {
-            string strNowTime = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            string strNowTime = DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss"); // 파일 형식 년 - 월 - 일 - 시 - 분 - 초
 
             if (Directory.Exists("log") == false)
-                Directory.CreateDirectory("log");
+                Directory.CreateDirectory("log"); // 폴더가 없을경우 폴더 생성
 
-            fileStream = new FileStream($"log\\log.{strNowTime}.txt", FileMode.CreateNew);
-            streamWrite = new StreamWriter(fileStream, System.Text.Encoding.UTF8);
+            fileStream = new FileStream($"log\\log.{strNowTime}.csv", FileMode.CreateNew); // 파일 생성
+            streamWrite = new StreamWriter(fileStream, System.Text.Encoding.UTF8); // 한글로 인코딩
         }
 
         public void Log(ELogLv eLogLv, string desc)
@@ -41,9 +41,9 @@ namespace Log_EX
                 return;
 
             System.Diagnostics.StackFrame stackFrame = new System.Diagnostics.StackFrame(1, true);
-            string funcName = stackFrame.GetMethod().Name;
-            string fileName = stackFrame.GetFileName();
-            string fileLine = stackFrame.GetFileLineNumber().ToString();
+            string funcName = stackFrame.GetMethod().Name; //프레임이 실행 되고 있는 메서드 가져오기
+            string fileName = stackFrame.GetFileName(); // 실행 중인 코드를 포함하는 파일 이름 가져오기 (실행 파일의 디버깅 기호에서 추출)
+            string fileLine = stackFrame.GetFileLineNumber().ToString(); // 실행중인 코드를 포함하는 파일에서 줄 번호 가져오기 ( 실행파일의 디버깅 기호에서 추출).스택 추적 생성
 
             // ,(콤마)는 .csv 파일에서 구분문자로 사용
             desc = desc.Replace(",", ".");
@@ -51,7 +51,7 @@ namespace Log_EX
             desc = $"{eLogLv.ToString()},\t{desc},\t{funcName},\t{fileName} {fileLine},\t{DateTime.Now}";
 
             streamWrite.WriteLine(desc);
-            streamWrite.Flush();
+            streamWrite.Flush();// 이 스트림의 버퍼를 지우고 버퍼링된 모든 데이터가 파일에 쓰여지도록 함
             fileStream.Flush();
 
             Int64 fileSize = fileStream.Length;
